9f5add0184e57337b3d136cef27bc14c725953ca
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-02-19 16:54:37 +0000

    LPF-653 - setup Jest

---
+++ src/middleware/axiosSetup.test.js
@@ -0,0 +1,21 @@
+import { axiosMiddleware } from "./axiosSetup";
+
+describe('axiosSetup middleware',  () => {
+
+    let req, res, next;
+
+    beforeEach(() => {
+        req = { 'get': jest.fn(), 'protocol': 'http' }
+        res = {}
+        next = jest.fn(); 
+    });
+
+    it('should call next() after setting up axios', () => {
+        req.get.mockReturnValue('localhost');
+
+        axiosMiddleware(req, res, next);
+
+        expect(next).toHaveBeenCalled();
+    })
+
+})
