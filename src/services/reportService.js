d6091c2e5fe9555e0c5985acce3069fff843c38c
Barry Nesbeth <barry.nesbeth@digital.justice.gov.uk>
2025-03-07 11:27:41 +0000

    created apiClient.js created apiClient.test.js amended reportService.js to use apiClient.js

--- src/services/reportService.js
+++ src/services/reportService.js
@@ -1,17 +1,10 @@
-import axios from 'axios';
-import config from '../../config.js';
+import apiClient from '../api/apiClient.js';
 
 /**
  * Gets the list of reports from the API.
- * The API endpoint is built using config.API_PROTOCOL and config.API_HOST.
  */
+
 export async function getReports() {
-    // Construct the base URL using your config
-    const baseURL = `${config.API_PROTOCOL}://${config.API_HOST}`;
-
-    // Make the HTTP GET request to the /reports endpoint
-    const response = await axios.get(`${baseURL}/reports`);
-
-    // Return the response data (which should be in the form { reportList: [...] })
+    const response = await apiClient.get('/reports');
     return response.data;
 }
